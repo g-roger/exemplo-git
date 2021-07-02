5232ccf4a9d46751cec9433ce9bf50fbb47fb72d
Gabriel Roger <gabriel.nroger@gmail.com>
2021-07-02 14:45:42 -0300

    Adicionar função depositar

--- banco.py
+++ banco.py
@@ -1,3 +1,9 @@
 def sacar(saldo, valor_de_saque):
     if valor_de_saque >= 0:
         return saldo - valor_de_saque
+
+
+def depositar(saldo, valor_de_deposito):
+    if valor_de_deposito <= 1000:
+        return saldo + valor_de_deposito
+    return saldo
